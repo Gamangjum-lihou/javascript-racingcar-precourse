5fd96a47e1593fbf2d6a9b065ac0c3049ab07d3f
SWARVY <swarvy0826@naver.com>
2022-12-11 21:06:50 +0900

    feat : 출력부 기능 구현 완료

--- src/View/View.js
+++ src/View/View.js
@@ -1,21 +1,44 @@
 const { Console } = require('@woowacourse/mission-utils');
 
+const MESSAGE = {
+  read_car_name: '자동차 이름을 5자 이하로 콤마로 구분하여 입력해주세요.\n',
+  read_try_count: '\n시도할 횟수를 입력해주세요.\n',
+  print_move_result: '\n실행 결과\n',
+  print_game_result: '최종 우승자: ',
+};
+
 const view = {
-  readCarName(callback) {
-    Console.readLine('', (input) => {
+  readCarName(callback, comeback) {
+    Console.readLine(MESSAGE.read_car_name, (input) => {
       callback(input);
     });
   },
 
-  readTryCount(callback) {
-    Console.readLine('', (input) => {
+  readTryCount(callback, comeback) {
+    Console.readLine(MESSAGE.read_try_count, (input) => {
       callback(input);
     });
   },
 
-  printMoveResult() {},
+  printResultMesage() {
+    Console.print(MESSAGE.print_move_result);
+  },
 
-  printGameResult() {},
+  printMoveResult(name, current) {
+    Console.print(`${name}: ${current}`);
+  },
+
+  printGameResult(winner) {
+    Console.print(`${MESSAGE.print_game_result}${winner}`);
+  },
+
+  printNewLine() {
+    Console.print('');
+  },
+
+  exit() {
+    return Console.close();
+  },
 };
 
 module.exports = view;
