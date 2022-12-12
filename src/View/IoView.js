b21cab3fa86376ce2679265a22697cb7e7e73469
minjaeKim <smallkdb@gmail.com>
2022-12-12 20:05:52 +0900

    feat(IoView): 입출력 코드 작성

---
+++ src/View/IoView.js
@@ -0,0 +1,26 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const IoView = {
+  readCarName(callback) {
+    Console.readLine('자동차 이름을 5자 이하로 콤마로 구분하여 입력해주세요.\n', (input) =>
+      callback(input)
+    );
+  },
+  readTryNumber(callback) {
+    Console.readLine('시도할 횟수를 입력해주세요.\n', (input) => callback(input));
+  },
+  printResultTitle() {
+    Console.print('실행결과\n');
+  },
+  printResult({ name, distance }) {
+    Console.print(`${name}: ${distance}`);
+  },
+  printWinner(names) {
+    Console.print(`최종 우승자: ${names}`);
+  },
+  printNewLine() {
+    Console.print('');
+  },
+};
+
+module.exports = IoView;
