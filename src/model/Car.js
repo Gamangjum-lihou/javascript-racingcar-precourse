31f5579ebb52c3c0e82956fa7680a1330f051186
minjaeKim <smallkdb@gmail.com>
2022-12-12 20:13:03 +0900

    feat(Car): 자동차의 이름과 이동칸수를 저장할 클래스 선언

---
+++ src/model/Car.js
@@ -0,0 +1,23 @@
+class Car {
+  #name;
+
+  #distance;
+
+  constructor(name) {
+    this.#name = name;
+    this.#distance = 0;
+  }
+
+  move(moveValue) {
+    if (moveValue >= 4) {
+      this.#distance += 1;
+    }
+    return this.#distance;
+  }
+
+  getName() {
+    return this.#name;
+  }
+}
+
+module.exports = Car;
